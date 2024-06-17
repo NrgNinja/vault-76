bad717f44c5f2fa859724a1684cce7bba192d8cd
NrgNinja <diazrenato2001@outlook.com>
2024-06-17 16:19:52 -0500

    updated hash_generator
    swapped the for loop for hash generation to hash_generator.rs, now it calls that file's function. updated comments as well

---
+++ entrance/target/debug/build/blake3-3261912354696b32/build_script_build-3261912354696b32.d
@@ -0,0 +1,5 @@
+/Users/renatodiaz/Documents/GitHub/vault-76/entrance/target/debug/build/blake3-3261912354696b32/build_script_build-3261912354696b32: /Users/renatodiaz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/blake3-1.5.1/build.rs
+
+/Users/renatodiaz/Documents/GitHub/vault-76/entrance/target/debug/build/blake3-3261912354696b32/build_script_build-3261912354696b32.d: /Users/renatodiaz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/blake3-1.5.1/build.rs
+
+/Users/renatodiaz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/blake3-1.5.1/build.rs:
---
+++ entrance/target/debug/deps/clap_lex-0a264a18c591383d.d
@@ -0,0 +1,7 @@
+/Users/renatodiaz/Documents/GitHub/vault-76/entrance/target/debug/deps/libclap_lex-0a264a18c591383d.rmeta: /Users/renatodiaz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap_lex-0.2.4/src/lib.rs
+
+/Users/renatodiaz/Documents/GitHub/vault-76/entrance/target/debug/deps/libclap_lex-0a264a18c591383d.rlib: /Users/renatodiaz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap_lex-0.2.4/src/lib.rs
+
+/Users/renatodiaz/Documents/GitHub/vault-76/entrance/target/debug/deps/clap_lex-0a264a18c591383d.d: /Users/renatodiaz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap_lex-0.2.4/src/lib.rs
+
+/Users/renatodiaz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap_lex-0.2.4/src/lib.rs:
