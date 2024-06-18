c3b2acd2b26776d63c745639dc49660190cce145
NrgNinja <diazrenato2001@outlook.com>
2024-06-18 11:19:00 -0500

    Added multithreaded support
    Reworked main.rs so that it can now ask how many threads to use to generate hashes.

--- entrance/src/main.rs
+++ entrance/src/main.rs
@@ -9,7 +9,9 @@
 // ignore unused code
 #[allow(dead_code)]
 use clap::{App, Arg};
+use std::sync::{Arc, Mutex};
 use std::time::Instant;
+use std::thread;
 
 mod hash_sorter;
 mod store_file;
@@ -21,36 +23,82 @@ fn main() {
         .about("Generates hashes for specified nonces using BLAKE3")
         .arg(
             Arg::with_name("nonces")
-                .short('n') // you can change this flag
+                .short('n') // cmd line flag
                 .long("nonces")
                 .takes_value(true) // there must be a number inputted
                 .help("Number of nonces to generate hashes for"),
         )
         .arg(
             Arg::with_name("filename")
-                .short('f') // you can change this flag
+                .short('f') // cmd line flag
                 .long("filename")
                 .takes_value(true) // there must be a filename inputted
                 .help("Output file to store the generated hashes"),
+        )
+        .arg(
+            Arg::with_name("threads")
+                .short('t') // cmd line flag
+                .long("threads")
+                .takes_value(true)
+                .default_value("1") // there must be threads specified
+                .help("Number of threads to use for hash generation"),
         )
         .get_matches();
 
     let num_nonces = matches
         .value_of("nonces")
         .unwrap_or("10") // default value if none specified
-        .parse::<u64>() // parse it into 64 bit unsigned int
+        .parse::<usize>() // parse it into 64 bit unsigned int
         .expect("Please provide a valid number for nonces");
 
+    let num_threads = matches
+        .value_of("threads")
+        .unwrap_or("4")
+        .parse::<usize>()
+        .expect("Please provide a valid number for threads");
+
+        
+    // output file to store binary format of hashes
     let output_file = matches.value_of("filename").unwrap_or("main.bin");
+
+    // shared resource among threads
+    let hashes = Arc::new(Mutex::new(Vec::new()));
+        
+    // vector to hold the thread handles
+    let mut handles = vec![];
 
     // start the timer
     let start = Instant::now();
 
-    // calls hash_generator.rs
-    let mut hashes = hash_generator::generate_hashes(num_nonces);
+    // spawning threads to use
+    for i in 0..num_threads {
+        let thread_hashes = hashes.clone();
+        handles.push(thread::spawn(move || {
+            println!("Thread {} has ID {:?}", i, thread::current().id()); // view the threads
+            let per_thread = (num_nonces + num_threads - 1) / num_threads; // ensure full coverage of nonces
+            let thread_nonce_start = i * per_thread;
+            let thread_nonce_end = std::cmp::min(thread_nonce_start + per_thread, num_nonces);
+
+            let mut local_hashes = Vec::new();
+            for nonce in thread_nonce_start..thread_nonce_end {
+                local_hashes.extend(hash_generator::generate_hashes(nonce as u64));
+            }
+
+            let mut hashes = thread_hashes.lock().unwrap(); // lock once per thread after work is done
+            hashes.extend(local_hashes);
+        }));
+    }
+
+    // wait for all threads to finish
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // lock the mutex one final time to sort and store hashes
+    let mut final_hashes = hashes.lock().unwrap();
 
     // calls hash_sorter.rs
-    hash_sorter::sort_hashes(&mut hashes);
+    hash_sorter::sort_hashes(&mut final_hashes);
 
     // // for viewing the generated hashes
     // for (nonce, hash) in hashes {
@@ -58,7 +106,7 @@ fn main() {
     // }
 
     // calls store_file.rs
-    match store_file::store_hashes(&hashes, output_file) {
+    match store_file::store_hashes(&final_hashes, output_file) {
         Ok(_) => println!("Hashes successfully written to {}", output_file),
         Err(e) => eprintln!("Error writing hashes to file: {}", e),
     }
---
+++ entrance/target/release/deps/entrance-92908887cefe0c18.d
@@ -0,0 +1,8 @@
+/Users/renatodiaz/Documents/GitHub/vault-76/entrance/target/release/deps/entrance-92908887cefe0c18: src/main.rs src/hash_sorter.rs src/store_file.rs src/hash_generator.rs
+
+/Users/renatodiaz/Documents/GitHub/vault-76/entrance/target/release/deps/entrance-92908887cefe0c18.d: src/main.rs src/hash_sorter.rs src/store_file.rs src/hash_generator.rs
+
+src/main.rs:
+src/hash_sorter.rs:
+src/store_file.rs:
+src/hash_generator.rs:
