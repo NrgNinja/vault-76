c6cdbac841bd396e9002069fc9e32059ac04f024
Ubuntu <ubuntu@torusnode17.maas>
2024-06-18 14:31:16 +0000

    changed sort

--- entrance/src/hash_sorter.rs
+++ entrance/src/hash_sorter.rs
@@ -2,5 +2,5 @@ use crate::Record;
 
 // this file will sort the hashes in a sequential pattern
 pub fn sort_hashes(hashes: &mut Vec<Record>) {
-    hashes.sort_by(|a, b| a.nonce.cmp(&b.nonce));
+    hashes.sort_by(|a, b| a.hash.cmp(&b.hash));
 }
