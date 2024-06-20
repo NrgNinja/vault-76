185b38e4325196b2f775fb5a178f0a1ece11d7a1
Varvara Bondarenko <varvara.varejka2014@gmail.com>
2024-06-20 17:10:31 -0500

    change

--- entrance/src/hash_generator.rs
+++ entrance/src/hash_generator.rs
@@ -1,47 +1,18 @@
-// // this file will store the hash generation process of the vault
-// use crate::Record;
-// use blake3::Hasher;
-// // use blake3::Hash;
-
-// const NONCE_SIZE: usize = 6;
-// const HASH_SIZE: usize = 26;
-
-// pub fn generate_hashes(num_records: u64) -> Vec<Record> {
-//     let mut hashes: Vec<Record> = Vec::new();
-
-//     for nonce in 0..num_records {
-//         // convert nonce to 6-byte array
-//         let nonce_bytes = (nonce as u64).to_be_bytes();
-//         let nonce_6_bytes: [u8; NONCE_SIZE] = nonce_bytes[2..8].try_into().unwrap(); // extract the lower 6 bytes as u8 array
-
-//         let mut hasher = Hasher::new();
-//         hasher.update(&nonce_6_bytes); // generate hash
-//         let hash = hasher.finalize();
-//         let hash = hash.to_string();
-//         let hash_slice = &hash[0..HASH_SIZE];
-//         let hash_slice = String::from(hash_slice);
-
-//         hashes.push(Record {
-//             nonce,
-//             hash: hash_slice,
-//         });
-//     }
-
-//     hashes
-// }
-
-// This file will store the hash generation process of the vault
-use crate::Record;
+// this file will store the hash generation process of the vault
 use blake3::Hasher;
-use rayon::prelude::*;
+
+use crate::Record;
 
 const NONCE_SIZE: usize = 6;
 const HASH_SIZE: usize = 26;
 
-// Generates hashes in parallel using Rayon
-pub fn generate_hashes(num_records: usize) -> Vec<Record> {
-    (0..num_records).into_par_iter().map(|nonce| {
-        // Convert nonce to 6-byte array
+pub fn generate_hashes(num_records: u64) -> Vec<Record> {
+    let capacity = (num_records * 32).try_into().unwrap();
+    let mut hashes: Vec<Record> = Vec::with_capacity(capacity);
+    // let mut hashes: Vec<Record> = Vec::new();
+
+    for nonce in 0..num_records {
+        // convert nonce to 6-byte array
         let nonce_bytes = (nonce as u64).to_be_bytes();
         let nonce_6_bytes: [u8; NONCE_SIZE] = nonce_bytes[2..8].try_into().unwrap(); // extract the lower 6 bytes as u8 array
 
@@ -52,9 +23,12 @@ pub fn generate_hashes(num_records: usize) -> Vec<Record> {
         let hash_slice = &hash[0..HASH_SIZE];
         let hash_slice = String::from(hash_slice);
 
-        Record {
-            nonce: nonce as u64,
+        hashes.push(Record {
+            nonce,
             hash: hash_slice,
-        }
-    }).collect()
+        });
+    }
+    println!("{}", hashes.len());
+
+    hashes
 }
--- entrance/src/main.rs
+++ entrance/src/main.rs
@@ -1,6 +1,5 @@
-// this file is the main driver of the vault codebase
+// this file will hold the main driver of our vault codebase
 use clap::{App, Arg};
-use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::time::Instant;
 
@@ -16,71 +15,55 @@ struct Record {
 }
 
 fn main() {
-    // defines letters for arguments that the user can call from Command Line
-    let matches = App::new("Vault-76")
-        .version("2.0")
-        .about("Generates hashes for unique nonces using BLAKE3 hashing function. This vault also has the ability to store each record (nonce/hash pair) into a vector, sort them accordingly, and even look them up efficiently.")
+    // Defines letters for arguments that the user can call from Command Line
+    let matches = App::new("Vault")
+        .version("1.0")
+        .about("Generates hashes for specified nonces using BLAKE3")
         .arg(
             Arg::with_name("nonces")
-                .short('n') // cmd line flag
+                .short('n') // you can change this flag
                 .long("nonces")
                 .takes_value(true) // there must be a number inputted
                 .help("Number of nonces to generate hashes for"),
         )
         .arg(
             Arg::with_name("filename")
-                .short('f') // cmd line flag
+                .short('f') // you can change this flag
                 .long("filename")
                 .takes_value(true) // there must be a filename inputted
                 .help("Output file to store the generated hashes"),
         )
         .arg(
             Arg::with_name("print")
-                .short('p') // cmd line flag
+                .short('p') // you can change this flag to whatever you want filename to represent
                 .long("print")
-                .takes_value(true) // there must be a number inputted
+                .takes_value(true) // there must be a filename inputted
                 .help("Number of records to print"),
         )
         .arg(
             Arg::with_name("sorting_on")
-                .short('s') // cmd line flag
+                .short('s') // you can change this flag to whatever you want filename to represent
                 .long("sorting_on")
-                .takes_value(true) // there must be a boolean inputted
+                .takes_value(true) // there must be a filename inputted
                 .help("Turn sorting on/off"),
-        )
-        .arg(
-            Arg::with_name("threads")
-                .short('t') // cmd line flag
-                .long("threads")
-                .takes_value(true)
-                .default_value("1") // there must be threads specified
-                .help("Number of threads to use for hash generation"),
         )
         .get_matches();
 
-    // variable to store the number of records to generate
+    // Defines a variable to store the number of records to generate
     let num_records = matches
         .value_of("nonces")
         .unwrap_or("10") // default value if none specified
-        .parse::<usize>() // parse it into 64 bit unsigned int
+        .parse::<u64>() // parse it into 64 bit unsigned int
         .expect("Please provide a valid number for nonces");
 
-    // variable to store the number of threads to use
-    let num_threads = matches
-        .value_of("threads")
-        .unwrap_or("4")
-        .parse::<usize>()
-        .expect("Please provide a valid number for threads");
+    let output_file = matches.value_of("filename").unwrap_or("");
 
-    // variable to store the number of records to print
+    // Defines a variable to store the number of hashes to print
     let num_records_to_print = matches
         .value_of("print")
         .unwrap_or("0")
         .parse::<u64>()
         .expect("Please provide a valid number of records to print");
-
-    // output file to store binary format of hashes
-    let output_file = matches.value_of("filename").unwrap_or("output.bin");
 
     // Defines a variable to check if the sorting mechanism should happen or not
     let sorting_on = matches
@@ -89,29 +72,23 @@ fn main() {
         .parse::<bool>()
         .expect("Please provide a valid value for sorting_on (true/false)");
 
-    // libary to use multiple threads
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(num_threads)
-        .build_global()
-        .unwrap();
-
+    // Start the timer
     let start = Instant::now();
 
-    // generate hashes in parallel
-    let mut hashes: Vec<Record> = (0..num_records)
-        .into_par_iter()
-        .flat_map(|nonce| hash_generator::generate_hashes(nonce)) // Cast usize to u64 here
-        .collect();
+    // Calls a function that generates hashes and saves them into Vec<Record>
+    let mut hashes = hash_generator::generate_hashes(num_records);
 
-    // Calls a function that sorts hashes in memory (hash_sorter.rs)
+    // Calls a function that sorts hashes in memory
     if sorting_on {
         hash_sorter::sort_hashes(&mut hashes);
     }
 
     // Calls store_hashes function to serialize generated hashes into binary and store them on disk
-    match store_file::store_hashes(&hashes, output_file) {
-        Ok(_) => println!("Hashes successfully written to {}", output_file),
-        Err(e) => eprintln!("Error writing hashes to file: {}", e),
+    if output_file != "" {
+        match store_file::store_hashes(&hashes, output_file) {
+            Ok(_) => println!("Hashes successfully written to {}", output_file),
+            Err(e) => eprintln!("Error writing hashes to file: {}", e),
+        }
     }
 
     // Calls print_records function to deserialize and print all of the records into command prompt
