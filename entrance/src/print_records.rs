6dda69c1841fbf68656034f44f6688717aca47be
NrgNinja <diazrenato2001@outlook.com>
2024-06-25 16:02:19 -0500

    translated printed records into decimal and hex format
    nonces now appear in decimal format and hashes now appear in hex format, all in an ordered manner

--- entrance/src/print_records.rs
+++ entrance/src/print_records.rs
@@ -3,17 +3,35 @@ use bincode::deserialize_from;
 use std::fs::File;
 use std::io::{self, BufReader};
 
+fn nonce_to_decimal(nonce: &[u8; 6]) -> u64 {
+    let mut num: u64 = 0;
+    for &byte in nonce.iter() {
+        num = num * 256 + byte as u64;
+    }
+    num
+}
+
+fn hash_to_string(hash: &[u8; 26]) -> String {
+    hash.iter().map(|b| format!("{:02x}", b)).collect::<Vec<String>>().join("")
+}
+
 // function to deserialize and print all of the records into command line
 pub fn print_records(filename: &str, num_records_print: u64) -> io::Result<()> {
     let file = File::open(filename)?;
     let mut reader = BufReader::new(file);
+
+    println!("Here are the first {num_records_print} records:");
+    println!("{:<20} | {:<64}", "Nonce (Decimal)", "Hash (Hex)");
+    println!("{}", "-".repeat(88)); // Creates a separator line
 
     let mut counter = 0;
 
     while counter < num_records_print {
         match deserialize_from::<&mut BufReader<File>, Record>(&mut reader) {
             Ok(record) => {
-                println!("{:?}", record);
+                let nonce_decimal = nonce_to_decimal(&record.nonce);
+                let hash_hex = hash_to_string(&record.hash);
+                println!("{:<20} | {}", nonce_decimal, hash_hex);
                 counter += 1;
             }
             Err(_) => break,
