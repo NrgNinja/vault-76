47e128862b003897086385d603531bcf0a7cad89
Ubuntu <ubuntu@torusnode17.maas>
2024-06-28 21:49:19 +0000

    changes

--- benchmark.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(10, 6))
-plt.plot([1, 2, 3], [1, 4, 9], marker='o')
-plt.xlabel('X Axis')
-plt.ylabel('Y Axis')
-plt.title('Test Plot')
-plt.show()
