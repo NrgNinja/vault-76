9f03793122b79279333094735c6c4a99d59e5982
Ubuntu <ubuntu@torusnode17.maas>
2024-07-01 21:33:01 +0000

    back to previous sorting

--- entrance/src/run.sh
+++ entrance/src/run.sh
@@ -2,6 +2,7 @@ free > /dev/null && sync > /dev/null && sudo sh -c 'echo 3 > /proc/sys/vm/drop_c
 sleep 5
 k="$1"
 threads="$2"
+print="$3"
 cd .. 
-./target/release/entrance -k $k -t $threads -s true -f output.bin -p 10
+./target/release/entrance -k $k -t $threads -s true -f output.bin -p $print
 # cargo run --release -- -k $k -t 16
