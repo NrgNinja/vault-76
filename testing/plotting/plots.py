40adf15e4fb6c562ea850851720faafbf9a1e6f5
NrgNinja <diazrenato2001@outlook.com>
2024-07-01 13:48:54 -0500

    Update plots.py
    Just changed up some numbers. Nothing crazy

--- testing/plotting/plots.py
+++ testing/plotting/plots.py
@@ -15,8 +15,13 @@ def plot_performance(threads, gen_times, write_times):
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_xlabel('Number of Threads')
     ax.set_ylabel('Time (seconds)')
+    # ax.set_title('1 Million Records on Mystic (Ubuntu)')
     # ax.set_title('33 Million Records on Mystic (Ubuntu)')
-    ax.set_title('33 Million Records on Macbook Air (M2)')
+    ax.set_title('100 Million Records on Mystic (Ubuntu)')
+    # ax.set_title('1 Million Records on Macbook Air (M2)')
+    # ax.set_title('33 Million Records on Macbook Air (M2)')
+    # ax.set_title('100 Million Records on Macbook Air (M2)')
+    
     ax.set_xticks(index + bar_width / 2)
     ax.set_xticklabels(threads)
     ax.legend()
@@ -27,10 +32,10 @@ def plot_performance(threads, gen_times, write_times):
 # Sample data - replace these with your actual data
 threads = ['1', '2', '4', '8', '16']
 # ubuntu
-# gen_times = [11.5, 6.8, 5.1, 3.2, 2.4]  # Example generation times
-# write_times = [5.9, 3.5, 2.3, 2.1, 2.3]  # Example write-to-disk times
+gen_times = [47.0, 27.7, 20.2, 14.6, 11.3]  # Example generation times
+write_times = [23.3, 11.8, 7.5, 7.5, 8]  # Example write-to-disk times
 # mac
-gen_times = [7.2, 3.9, 2.6, 1.9, 2.0]  # Example generation times
-write_times = [29.0, 28.8, 25.3, 26.4, 26.5]  # Example write-to-disk times
+# gen_times = [7.2, 3.9, 2.6, 1.9, 2.0]  # Example generation times
+# write_times = [29.0, 28.8, 25.3, 26.4, 26.5]  # Example write-to-disk times
 
 plot_performance(threads, gen_times, write_times)
