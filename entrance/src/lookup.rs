d4b7bab986fe88a27c8b2f5395a6ff7dc502661a
Ubuntu <ubuntu@torusnode17.maas>
2024-07-08 21:06:36 +0000

    Lookup that takes 8ms - testing needed

--- entrance/src/lookup.rs
+++ entrance/src/lookup.rs
@@ -1,18 +1,11 @@
 use std::{
     cmp::Ordering,
-    fs::{DirEntry, File},
+    fs::File,
     io::{self, BufRead, BufReader},
-    sync::{
-        atomic::{AtomicBool, Ordering as AtomicOrdering},
-        Arc, Mutex,
-    },
     time::Instant,
 };
 
 use memmap2::Mmap;
-use rayon::iter::{
-    IntoParallelIterator, IntoParallelRefIterator, ParallelIterator,
-};
 
 use crate::{Record, RECORD_SIZE};
 
@@ -31,46 +24,8 @@ pub fn lookup_hash_in_file(directory: &str, target_hash: &str) -> io::Result<Opt
 
     let file_index = read_file_index(directory)?;
 
-    // Check if the directory exists
-    // if !fs::metadata(directory).is_ok() {
-    //     eprintln!("Directory does not exist: {}", directory);
-    //     return Ok(None);
-    // }
-
-    // Very slow!!!!!
-    // let get_file_names_start = Instant::now();
-    // let entries: Vec<DirEntry> = match std::fs::read_dir(directory) {
-    //     Ok(entries) => entries.filter_map(Result::ok).collect(),
-    //     Err(e) => {
-    //         eprintln!("Failed to read directory: {}", e);
-    //         return Ok(None);
-    //     }
-    // };
-    // let entries: Vec<DirEntry> = fs::read_dir(directory)?
-    //     .par_bridge()
-    //     .filter_map(Result::ok)
-    //     .collect();
-    // let get_file_names_duration = get_file_names_start.elapsed();
-    // println!(
-    //     "All the files names acquired in {:?}",
-    //     get_file_names_duration
-    // );
-
     for entry in file_index {
-        // let start_entry = Instant::now();
-        // let file_name = entry.file_name();
-        // let file_name = file_name.to_str().unwrap();
-
         println!("Checking file: {}", entry.filename);
-
-        // Extract the hash range from the file name
-        // let parts: Vec<&str> = file_name.trim_end_matches(".bin").split('-').collect();
-        // if parts.len() != 2 {
-        //     continue;
-        // }
-
-        // let start_hash = hex::decode(parts[0]).expect("Invalid hex string in file name");
-        // let end_hash = hex::decode(parts[1]).expect("Invalid hex string in file name");
 
         // Truncate start_hash and end_hash to the length of target_hash_bytes
         let truncated_start_hash = &entry.start_hash[..target_hash_len];
@@ -164,11 +119,9 @@ fn read_file_index(directory: &str) -> io::Result<Vec<FileIndexEntry>> {
     for line in reader.lines() {
         let line = line?;
         let line_trimmed = line.trim();
-        println!("Line that was extracted from file_index.bin: {}", line_trimmed);
 
         let parts: Vec<&str> = line_trimmed.split_whitespace().collect();
         if parts.len() == 3 {
-            println!("Parts: {:?}", parts);
             let filename = parts[0].to_string();
             let start_hash = hex::decode(parts[1]).expect("Invalid hex string in file index");
             let end_hash = hex::decode(parts[2]).expect("Invalid hex string in file index");
@@ -179,107 +132,105 @@ fn read_file_index(directory: &str) -> io::Result<Vec<FileIndexEntry>> {
             });
         } else {
             eprintln!("Skipping line due to incorrect format: {}", line_trimmed);
-        
         }
     }
-    println!("File index contents: {:?}", index_entries);
 
     Ok(index_entries)
 }
 
-fn linear_search(
-    num_records: usize,
-    file_name: &str,
-    buffer: &[u8],
-    entry: &DirEntry,
-    results: &mut Vec<Record>,
-    target_hash_bytes: &Vec<u8>,
-    // start_looking_inside: Instant,
-) -> Vec<Record> {
-    for i in 0..num_records {
-        // let record_start = i * record_size;
-        // let record_end = record_start + record_size;
-        // let record_bytes = &buffer[record_start..record_end];
-        // println!(
-        //     "Checking record hash: {:?}",
-        //     &buffer[i * record_size..(i + 1) * record_size]
-        // );
+// fn linear_search(
+//     num_records: usize,
+//     file_name: &str,
+//     buffer: &[u8],
+//     entry: &DirEntry,
+//     results: &mut Vec<Record>,
+//     target_hash_bytes: &Vec<u8>,
+//     // start_looking_inside: Instant,
+// ) -> Vec<Record> {
+//     for i in 0..num_records {
+//         // let record_start = i * record_size;
+//         // let record_end = record_start + record_size;
+//         // let record_bytes = &buffer[record_start..record_end];
+//         // println!(
+//         //     "Checking record hash: {:?}",
+//         //     &buffer[i * record_size..(i + 1) * record_size]
+//         // );
 
-        let record: Record =
-            match bincode::deserialize(&buffer[i * RECORD_SIZE..(i + 1) * RECORD_SIZE]) {
-                Ok(record) => record,
-                Err(_) => {
-                    eprintln!(
-                        "Failed to deserialize record at index {} in file {}",
-                        i, file_name
-                    );
-                    continue;
-                }
-            };
+//         let record: Record =
+//             match bincode::deserialize(&buffer[i * RECORD_SIZE..(i + 1) * RECORD_SIZE]) {
+//                 Ok(record) => record,
+//                 Err(_) => {
+//                     eprintln!(
+//                         "Failed to deserialize record at index {} in file {}",
+//                         i, file_name
+//                     );
+//                     continue;
+//                 }
+//             };
 
-        // Check if the current record's hash starts with the target hash prefix
-        if record.hash.starts_with(&target_hash_bytes) {
-            // let looking_inside_duration = start_looking_inside.elapsed();
-            // println!(
-            //     "Traversing over the contents of a file took: {:?}",
-            //     looking_inside_duration
-            // );
+//         // Check if the current record's hash starts with the target hash prefix
+//         if record.hash.starts_with(&target_hash_bytes) {
+//             // let looking_inside_duration = start_looking_inside.elapsed();
+//             // println!(
+//             //     "Traversing over the contents of a file took: {:?}",
+//             //     looking_inside_duration
+//             // );
 
-            let entry_string = entry.file_name().to_string_lossy().to_string();
-            println!("Record is in file: {:?}", entry_string);
-            results.push(record);
-            // return Ok(Some(record));
-        }
-    }
+//             let entry_string = entry.file_name().to_string_lossy().to_string();
+//             println!("Record is in file: {:?}", entry_string);
+//             results.push(record);
+//             // return Ok(Some(record));
+//         }
+//     }
 
-    results.to_vec()
-}
+//     results.to_vec()
+// }
 
-fn parallel_binary_search(
-    num_records: usize,
-    buffer: &[u8],
-    file_name: &str,
-    target_hash_len: usize,
-    target_hash_bytes: &[u8],
-) -> Option<Record> {
-    let chunk_size = 1024;
-    let num_chunks = (num_records + chunk_size - 1) / chunk_size;
+// fn parallel_binary_search(
+//     num_records: usize,
+//     buffer: &[u8],
+//     file_name: &str,
+//     target_hash_len: usize,
+//     target_hash_bytes: &[u8],
+// ) -> Option<Record> {
+//     let chunk_size = 1024;
+//     let num_chunks = (num_records + chunk_size - 1) / chunk_size;
 
-    (0..num_chunks).into_par_iter().find_map_any(|chunk_index| {
-        let mut left = chunk_index * chunk_size; // chunk_start
-        let mut right = (left + chunk_size).min(num_records) - 1; // chunk_end
+//     (0..num_chunks).into_par_iter().find_map_any(|chunk_index| {
+//         let mut left = chunk_index * chunk_size; // chunk_start
+//         let mut right = (left + chunk_size).min(num_records) - 1; // chunk_end
 
-        while left <= right {
-            let mid = (left + right) / 2;
-            let mid_record_start = mid * RECORD_SIZE;
-            let mid_record_end = mid_record_start + RECORD_SIZE;
-            let mid_record_bytes = &buffer[mid_record_start..mid_record_end];
+//         while left <= right {
+//             let mid = (left + right) / 2;
+//             let mid_record_start = mid * RECORD_SIZE;
+//             let mid_record_end = mid_record_start + RECORD_SIZE;
+//             let mid_record_bytes = &buffer[mid_record_start..mid_record_end];
 
-            let mid_record: Record = match bincode::deserialize(mid_record_bytes) {
-                Ok(record) => record,
-                Err(_) => {
-                    eprintln!(
-                        "Failed to deserialize record at index {} in file {}",
-                        mid, file_name
-                    );
-                    break;
-                }
-            };
+//             let mid_record: Record = match bincode::deserialize(mid_record_bytes) {
+//                 Ok(record) => record,
+//                 Err(_) => {
+//                     eprintln!(
+//                         "Failed to deserialize record at index {} in file {}",
+//                         mid, file_name
+//                     );
+//                     break;
+//                 }
+//             };
 
-            match mid_record.hash[..target_hash_len].cmp(&target_hash_bytes[..]) {
-                Ordering::Less => left = mid + 1,
-                Ordering::Greater => {
-                    if mid == 0 {
-                        break;
-                    }
-                    right = mid - 1;
-                }
-                Ordering::Equal => return Some(mid_record),
-            }
-        }
-        None
-    })
-}
+//             match mid_record.hash[..target_hash_len].cmp(&target_hash_bytes[..]) {
+//                 Ordering::Less => left = mid + 1,
+//                 Ordering::Greater => {
+//                     if mid == 0 {
+//                         break;
+//                     }
+//                     right = mid - 1;
+//                 }
+//                 Ordering::Equal => return Some(mid_record),
+//             }
+//         }
+//         None
+//     })
+// }
 
 fn binary_search(
     num_records: usize,
@@ -331,60 +282,60 @@ fn binary_search(
 }
 
 // Multi-threaded
-pub fn lookup_hash(directory: &str, target_hash: &str) -> io::Result<Option<Record>> {
-    let target_hash_bytes = hex::decode(target_hash).expect("Hash couldn't be converted to hex");
+// pub fn lookup_hash(directory: &str, target_hash: &str) -> io::Result<Option<Record>> {
+//     let target_hash_bytes = hex::decode(target_hash).expect("Hash couldn't be converted to hex");
 
-    // Read the contents of the directory
-    let paths: Vec<_> = std::fs::read_dir(directory)?
-        .filter_map(Result::ok)
-        .map(|entry| entry.path())
-        .collect();
+//     // Read the contents of the directory
+//     let paths: Vec<_> = std::fs::read_dir(directory)?
+//         .filter_map(Result::ok)
+//         .map(|entry| entry.path())
+//         .collect();
 
-    // Atomic flag to indicate if the record has been found
-    let record_found = Arc::new(AtomicBool::new(false));
-    // Mutex to store the found record
-    let found_record = Arc::new(Mutex::new(None));
+//     // Atomic flag to indicate if the record has been found
+//     let record_found = Arc::new(AtomicBool::new(false));
+//     // Mutex to store the found record
+//     let found_record = Arc::new(Mutex::new(None));
 
-    // Process each file in parallel
-    paths.par_iter().for_each(|path| {
-        if record_found.load(AtomicOrdering::Relaxed) {
-            return; // Early exit if record is already found
-        }
+//     // Process each file in parallel
+//     paths.par_iter().for_each(|path| {
+//         if record_found.load(AtomicOrdering::Relaxed) {
+//             return; // Early exit if record is already found
+//         }
 
-        let file = match File::open(path) {
-            Ok(f) => f,
-            Err(_) => return,
-        };
+//         let file = match File::open(path) {
+//             Ok(f) => f,
+//             Err(_) => return,
+//         };
 
-        let mmap = unsafe { Mmap::map(&file).expect("Could not memory-map the file") };
+//         let mmap = unsafe { Mmap::map(&file).expect("Could not memory-map the file") };
 
-        let buffer = &mmap;
-        // if file.read_to_end(&mut buffer).is_err() {
-        //     return;
-        // }
+//         let buffer = &mmap;
+//         // if file.read_to_end(&mut buffer).is_err() {
+//         //     return;
+//         // }
 
-        let chunk_size = 32;
-        let chunks = buffer.chunks_exact(chunk_size);
+//         let chunk_size = 32;
+//         let chunks = buffer.chunks_exact(chunk_size);
 
-        for chunk in chunks {
-            if record_found.load(AtomicOrdering::Relaxed) {
-                return; // Early exit if record is already found
-            }
+//         for chunk in chunks {
+//             if record_found.load(AtomicOrdering::Relaxed) {
+//                 return; // Early exit if record is already found
+//             }
 
-            let record: Record = match bincode::deserialize(chunk) {
-                Ok(r) => r,
-                Err(_) => return,
-            };
+//             let record: Record = match bincode::deserialize(chunk) {
+//                 Ok(r) => r,
+//                 Err(_) => return,
+//             };
 
-            if record.hash == target_hash_bytes.as_slice() {
-                let mut found = found_record.lock().unwrap();
-                *found = Some(record);
-                record_found.store(true, AtomicOrdering::Relaxed); // Set the flag to indicate the record has been found
-                return;
-            }
-        }
-    });
+//             if record.hash == target_hash_bytes.as_slice() {
+//                 let mut found = found_record.lock().unwrap();
+//                 *found = Some(record);
+//                 record_found.store(true, AtomicOrdering::Relaxed); // Set the flag to indicate the record has been found
+//                 return;
+//             }
+//         }
+//     });
 
-    let found = found_record.lock().unwrap();
-    Ok(*found)
-}
+//     let found = found_record.lock().unwrap();
+//     Ok(*found)
+// }
