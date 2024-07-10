c0aaaef9f5db2784542c952b710a4059633d1239
Ubuntu <ubuntu@torusnode17.maas>
2024-07-10 21:41:00 +0000

    Using serde/postcard instead of bincode to serialize

--- entrance/src/benchmark/generate_once.sh
+++ entrance/src/benchmark/generate_once.sh
@@ -12,17 +12,17 @@ rm -rf "${output_dir:?}"/*
 
 free >/dev/null && sync >/dev/null && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches' && free >/dev/null
 
-# sar -u 1 >stats/cpu/cpu-stats_$k$threads.txt &
-# sar -b 1 >stats/io/io-stats_$k$threads.txt &
-# sar -r 1 >stats/memory/memory-stats_$k$threads.txt &
+sar -u 1 >stats/cpu/cpu-stats_$k$threads.txt &
+sar -b 1 >stats/io/io-stats_$k$threads.txt &
+sar -r 1 >stats/memory/memory-stats_$k$threads.txt &
 sleep 5
 
-./target/release/entrance -k $k -t $threads
+./target/release/entrance -k $k -t $threads -p 100
 # dd if=/dev/urandom of=newfile bs=1M count=1024
 # shred -s 1000000000 - >my-file
 sleep 5
 
-# pkill sar
+pkill sar
 
 file_size=$(du -hs $output_dir)
 echo "The total size of all files is $file_size"
--- entrance/src/store_hashes.rs
+++ entrance/src/store_hashes.rs
@@ -1,27 +1,25 @@
+use crate::{Record, RECORD_SIZE};
+use heapless::Vec as HeaplessVec;
+use postcard::to_vec;
 use std::fs::OpenOptions;
 use std::io::{self, Write};
-
-use crate::Record;
-
-// pub fn create_sparse_file(filename: &str, size: u64) -> io::Result<()> {
-//     let file = OpenOptions::new().write(true).create(true).open(filename)?;
-//     file.set_len(size)?;
-//     // println!("{:?}", file);
-//     Ok(())
-// }
+use std::time::Instant;
 
 pub fn store_hashes_chunk(chunk: &[Record], filename: &str) -> io::Result<()> {
     let path = format!("output/{}", filename);
     let mut file = OpenOptions::new().write(true).create(true).open(path)?;
 
-    let record_size = 32;
-
     // Collect all serialized records into a single buffer
-    let mut buffer: Vec<u8> = Vec::with_capacity(chunk.len() * record_size);
+    let serialize_chunk_start = Instant::now();
+
+    let mut buffer: Vec<u8> = Vec::with_capacity(chunk.len() * RECORD_SIZE);
     for hash in chunk {
-        let encoded: Vec<u8> = bincode::serialize(hash).expect("Failed to serialize hash");
-        buffer.extend_from_slice(&encoded);
+        let encoded: HeaplessVec<u8, 32> = to_vec(hash).expect("Failed to serialize hash");
+        // let encoded: Vec<u8> = bincode::serialize(hash).expect("Failed to serialize hash");
+        buffer.extend_from_slice(&encoded)
     }
+    let serialize_chunk_duration = serialize_chunk_start.elapsed();
+    println!("Serializing took {:?}", serialize_chunk_duration);
 
     // Seek to the start position and write the entire buffer
     file.write_all(&buffer)?;
