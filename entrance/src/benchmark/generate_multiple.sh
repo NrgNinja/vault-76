b808aeff8bdc7ad7f567cf25b970dbdb1d724eb4
Varvara <vbondarenko@hawk.iit.edu>
2024-07-31 20:05:32 +0000

    Cargo clean

--- entrance/src/benchmark/generate_multiple.sh
+++ entrance/src/benchmark/generate_multiple.sh
@@ -15,7 +15,7 @@ for n in {1..10}; do
 
     free >/dev/null && sync >/dev/null && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches' && free >/dev/null
     sudo sync
-    sleep 5
+    sleep 1
 
     # Capture the output of the program
     ./../../target/release/entrance -k $k -t $threads -m $memory >>"vault_csv/vault_$k"_"$threads"t".csv"
