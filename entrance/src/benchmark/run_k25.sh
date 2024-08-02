e3e3a00b8872138b820dc3817580b9ab8f7de806
Varvara <vbondarenko@hawk.iit.edu>
2024-08-02 14:46:36 +0000

    Changes to run 25 script

--- entrance/src/benchmark/run_k25.sh
+++ entrance/src/benchmark/run_k25.sh
@@ -8,18 +8,18 @@ max_num_threads=32 #6 runs
 memory=$((1024 * 1024)) #1MB
 max_memory=$((1024 * 1024 * 1024))  # 1GB #11 runs
 k=25
-echo "threads,memory,hash_time,sort_time,sync_time">> "vault-76-k${k}.txt"
+echo "threads,memory,hash_time,sort_time,sync_time">> "vault76_k${k}_nvme.txt"
 
-for (( t=num_threads; t<=$max_num_threads; t=$((t * 2)) ))
+for (( t=num_threads; t<=max_num_threads; t *= 2 ))
 do
-    for (( m=memory; m<=$max_memory; m=$((m * 2)) ))
+    for (( m=memory; m<=max_memory; m *= 2 ))
     do
         # Remove the output file
         rm ../../output/output.bin
         echo "$k $t $m"
   
         # Run the cargo command with the current value of k and pipe the output to a file
-        echo -n "$t,$m," >> "vault-76-k${k}.txt"
-        cargo run --release -- -k "$k" -t "$t" -m "$m" >> "vault-76-k${k}.txt"
+        echo -n "$t,$m," >> "vault76_k${k}_nvme.txt"
+        cargo run --release -- -k "$k" -t "$t" -m "$m" >> "vault76_k${k}_nvme.txt"
     done
 done
