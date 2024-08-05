dfa2a363b138fc2a5f5e38acc861d622cc911adf
Varvara <vbondarenko@hawk.iit.edu>
2024-08-05 19:01:53 +0000

    generate_m changes

--- entrance/src/benchmark/generate_multiple.sh
+++ entrance/src/benchmark/generate_multiple.sh
@@ -7,16 +7,16 @@ k="$1"
 threads="$2"
 memory="$3"
 
-echo "Gen&Flush,Sort,Sync" >"vault_csv/vault_$k"_"$threads"t".csv"
+echo "Gen&Flush,Sort,Sync" >"vault_csv/vault_eightsocket_$k"_"$threads"t".csv"
 
-for n in {1..10}; do
+for n in {1..5}; do
     # Clean the output directory
     rm -rf "${output_dir:?}"/*
 
     free >/dev/null && sync >/dev/null && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches' && free >/dev/null
     sudo sync
-    sleep 5
+    sleep 1
 
     # Capture the output of the program
-    ./../../target/release/entrance -k $k -t $threads -m $memory >>"vault_csv/vault_$k"_"$threads"t".csv"
+    ./../../target/release/entrance -k $k -t $threads -m $memory >>"vault_csv/vault_eightsocket_$k"_"$threads"t".csv"
 done
